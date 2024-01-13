87abd1290887dd3129957177a6efe3c124cd90b7
thinlam <thinlam6@gmail.com>
2024-01-13 20:59:42 +0700

    chinh sua lan 3

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Char : MonoBehaviour
+
+
+public class PlayerCharacter : MonoBehaviour
 {
+    
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        Debug.Log(horizontal);
         Vector2 position = transform.position;
-        position.x = position.x + 0.25f;
+        position.x = position.x + 0.1f * horizontal;
         transform.position = position;
+        
     }
+
 }
